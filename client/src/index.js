a1abb8d0efa1135dbcd2a0cd5c64587081eb908c
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-04 11:15:40 +0530

    feat: home component added

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Login from './views/Login/Login';
 import Signup from './views/Signup/Signup';
+import Home from './views/Home/Home';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
   {
     path: '/login',
     element: <Login />
