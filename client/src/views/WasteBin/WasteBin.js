61b65e33f913ced438f17151711921471bdfdb8f
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-06 13:59:28 +0530

    add waste bin page design

--- client/src/views/WasteBin/WasteBin.js
+++ client/src/views/WasteBin/WasteBin.js
@@ -3,15 +3,38 @@ import './WasteBin.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 
-function WasteBin() {
+function WasteBins() {
   return (
-    <div>
-        <>
-        <Navbar />
-        <Footer />
-        </>
+    <>
+    <Navbar />
+    <div className="smart-waste-bins">
+      <section className="ferrous-bin">
+        <h2>Ferrous Smart Waste Bin</h2>
+        <p>
+          The Ferrous Smart Waste Bin is designed to collect ferrous materials such as iron and steel. It helps in recycling and managing these materials efficiently.
+        </p>
+        <ul>
+          <li>Capacity: 50 liters</li>
+          <li>Location: Industrial Area</li>
+          {/* Add more details specific to the Ferrous bin */}
+        </ul>
+        {/* Add components or functionality related to the Ferrous bin */}
+      </section>
+
+      <section className="non-ferrous-bin">
+        <h2>Non-Ferrous Smart Waste Bin</h2>
+        <p>
+          The Non-Ferrous Smart Waste Bin is dedicated to non-ferrous materials like aluminum, copper, and brass. It promotes eco-friendly disposal of these materials.
+        </p>
+        <ul>
+          <li>Capacity: 40 liters</li>
+          <li>Location: Residential Area</li>
+        </ul>
+      </section>
     </div>
-  )
+    <Footer />
+    </>
+  );
 }
 
-export default WasteBin
+export default WasteBins;
