d66750c19ff8d41e485b1fd3bde0347bafbe487f
bhartiharinkhede44 <bhartiharinkhede44@gmail.com>
2024-02-05 12:22:18 +0530

    login and signup api

---
+++ server/index.js
@@ -0,0 +1,78 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+const app = express();
+app.use(express.json());
+import User from './models/user.js'
+
+app.post('/signup', async (req, res) => {
+
+    const { name, email, mobile, address, password, roll } = req.body
+  
+    const newUser = new User({
+        name, email, mobile, address, password, roll
+    })
+  
+  
+  
+    try {
+        const saveUser = await newUser.save()
+        return res.json({
+            data: saveUser,
+            success: true,
+            message: "successfully created signup"
+        })
+    }
+    catch (e) {
+        return res.json(
+            {
+                message: (e.message)
+            }
+        )
+    }
+  })
+  app.get('/signup',async (req,res)=>{
+    const alluser = await User.find();
+    res.json({
+      data:alluser,
+      message: "feached all user"
+    })
+  })
+// Login 
+app.post('/login',async(req,res)=>{
+
+    const{  email,password}=req.body;
+
+    const loginUser = await User.findOne({email:email ,password:password}).select('name gmail roll address')
+
+
+    if(loginUser){
+        return res.json({
+            success:true,
+            data:loginUser,
+            message:"login successfully"
+
+        })
+    }
+   else{
+    return res.json({
+        success:false,
+        data:loginUser,
+
+
+    })
+
+   }
+})
+const PORT = process.env.PORT || 5000;
+
+const connectDB = async () => {
+  const conn = mongoose.connect(process.env.MONGODB_URI);
+  if (conn)
+    console.log('mongoDB connect succesfully')
+};
+app.listen(PORT, () => {
+  console.log(`server is running on ; ${PORT}`)
+  connectDB();
+})
---
+++ server/models/user.js
@@ -0,0 +1,38 @@
+import {model,Schema} from 'mongoose'
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        default:0,
+        unique:true,
+        required:true
+    },
+    address:{
+        type:String,
+
+    },
+    roll:{
+        type:String,
+        required:true
+        
+    }
+},{
+    timestamps:true,
+});
+
+const User = model('User',UserSchema)
+
+export default User
